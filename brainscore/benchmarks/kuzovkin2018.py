f4d8deab414548076f93864dcd6c464ff86fa471
hermessuen <hermessuen@gmail.com>
2021-07-10 15:40:08 -0400

    Update kuzovkin2018.py

--- brainscore/benchmarks/kuzovkin2018.py
+++ brainscore/benchmarks/kuzovkin2018.py
@@ -89,7 +89,7 @@ def AruKuzovkin2018PLS():
     new_assembly = assembly[:, idx, :]
 
     # Can replace the "name" of the brain region
-    new_assembly = type(new_assembly)(new_assembly.values, coords={coord: (dims, values if coord != 'region' else ['V1'] * len(new_assembly['region'])) for
+    new_assembly = type(new_assembly)(new_assembly.values, coords={coord: (dims, values if coord != 'region' else ['V2'] * len(new_assembly['region'])) for
                                    coord, dims, values in walk_coords(new_assembly)}, dims=new_assembly.dims)
 
 
